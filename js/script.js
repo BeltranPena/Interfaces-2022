9e04044598e252c28aaa1e16e3d6ca731943b35e
Guillermina Lauge <guillerminalauge@gmail.com>
2022-10-02 23:53:21 -0300

    Se crea carrousel principal

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ let vertNav = document.querySelector("#vertical-nav");
 menuBtn.addEventListener("click", () => {
     vertNav.classList.toggle("display-none");
 });
+
+
+
+//--------------- Main carousel -----------------------------
+const imgs = 2;
+let moveX = -(100/imgs);
+let cContainer = document.querySelector("#c-container");
+let cNavPoints = document.querySelectorAll(".c-nav-point");
+
+cNavPoints.forEach((point, i) => {
+    cNavPoints[i].addEventListener('click', () => {
+        let pos = i;
+        let op = pos*(moveX);
+        cContainer.style.transform = `translateX(${op}%)`;
+        cNavPoints.forEach((point) => {point.classList.remove("active");});
+        point.classList.add("active");     
+    });
+});
